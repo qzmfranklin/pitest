17b8aed1e1d994b95fc031e4468158375bdf74b1
Zhongming Qu <qzmfranklin@gmail.com>
2016-03-28 20:00:04 -0700

    Add README.rst to long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import os
 import setuptools
+
+# Get the long description from the README file
+this_dir = os.path.abspath(os.path.dirname(__file__))
+readme_fname = os.path.join(this_dir, 'README.rst')
+with open(readme_fname, encoding='utf-8') as f:
+    long_description = f.read()
 
 if __name__ == '__main__':
     setuptools.setup(
         name='pitest',
         version='0.1',
         description='An Object Oriented Testing Framework.',
+        long_description = long_description,
         url='https://github.com/qzmfranklin/pitest',
         author='Zhongming Qu',
         author_email='qzmfranklin@gmail.com',
